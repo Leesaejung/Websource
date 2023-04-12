4476609704478194ea266c6d893853d3870c7697
Saejung <leeesaejung@gmail.com>
2023-04-12 18:03:52 +0900

    2023년04월12일작업

---
+++ scriptsource/event/tab.js
@@ -0,0 +1,35 @@
+// var button = document.querySelectorAll(".tab-button");
+// var content = document.querySelectorAll(".tab-content");
+
+// button[0].addEventListener("click", function () {
+//   button[0].classList.remove("orange");
+//   button[1].classList.remove("orange");
+//   button[2].classList.remove("orange");
+//   button[0].classList.add("orange");
+//   content[0].classList.remove("show");
+//   content[1].classList.remove("show");
+//   content[2].classList.remove("show");
+//   content[0].classList.add("show");
+// });
+
+// button[1].addEventListener("click", function () {
+//   button[0].classList.remove("orange");
+//   button[1].classList.remove("orange");
+//   button[2].classList.remove("orange");
+//   button[1].classList.add("orange");
+//   content[0].classList.remove("show");
+//   content[1].classList.remove("show");
+//   content[2].classList.remove("show");
+//   content[1].classList.add("show");
+// });
+
+// button[2].addEventListener("click", function () {
+//   button[0].classList.remove("orange");
+//   button[1].classList.remove("orange");
+//   button[2].classList.remove("orange");
+//   button[2].classList.add("orange");
+//   content[0].classList.remove("show");
+//   content[1].classList.remove("show");
+//   content[2].classList.remove("show");
+//   content[2].classList.add("show");
+// });
